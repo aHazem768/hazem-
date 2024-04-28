407124cadedb9a1e6c1febfc37cdbe81de15b92e
aHazem768 <164690964+aHazem768@users.noreply.github.com>
2024-04-29 01:15:13 +0500

    Update pp.py

--- pp.py
+++ pp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 from PIL import Image
-import matplotlib.pyplot as plt
+
 import numpy as np
 import plotly.express as px
 from time import sleep
